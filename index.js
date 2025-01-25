b621dc8f83c97a74469b6dcf0404bdc7a9a19995
Dariusz <dariusz.skiba80@gmail.com>
2025-01-25 09:04:17 +0000

    Obsługa żadan i komunikacja z GoogleSheets

--- index.js
+++ index.js
@@ -22,6 +22,44 @@ app.use((req, res, next) => {
 
 app.use(express.json()); // Middleware do parsowania JSON request bodies
 
+// Funkcja do odświeżenia tokenu dostępu
+async function refreshAccessToken() {
+    const response = await axios.post(process.env.TOKEN_URI, {
+        client_id: process.env.CLIENT_ID,
+        client_secret: process.env.CLIENT_SECRET,
+        refresh_token: process.env.REFRESH_TOKEN,
+        grant_type: 'refresh_token',
+    });
+    return response.data.access_token;
+}
+
+// Endpoint do przesyłania danych do Google Sheets
+app.post('/api/submit-data', async (req, res) => {
+    const dataToSend = req.body.values;
+    console.log("Received data to submit:", dataToSend); // Logowanie danych
+    console.log("CLIENT_ID:", process.env.CLIENT_ID); // Logowanie CLIENT_ID
+    console.log("SPREADSHEET_ID:", process.env.SPREADSHEET_ID); // Logowanie SPREADSHEET_ID
+
+    try {
+        const accessToken = await refreshAccessToken(); // Odświeżenie tokenu dostępu
+        const response = await axios.post(
+            `https://sheets.googleapis.com/v4/spreadsheets/${process.env.SPREADSHEET_ID}/values/scaned_products!A:F:append?valueInputOption=USER_ENTERED&insertDataOption=INSERT_ROWS`,
+            { values: dataToSend },
+            {
+                headers: {
+                    'Authorization': `Bearer ${accessToken}`, // Użycie odświeżonego tokenu dostępu
+                    'Content-Type': 'application/json'
+                }
+            }
+        );
+        console.log("Data submitted successfully:", response.data); // Logowanie odpowiedzi
+        res.status(200).json({ message: 'Data submitted successfully!', response: response.data });
+    } catch (error) {
+        console.error('Error submitting data:', error.response ? error.response.data : error.message); // Logowanie błędu
+        res.status(500).json({ error: 'Error submitting data.' });
+    }
+});
+
 // Endpoint do uzyskiwania informacji o produkcie
 app.get('/api/:barcode', async (req, res) => {
     try {
@@ -55,33 +93,6 @@ app.post('/csp-report', express.json({ type: 'application/csp-report' }), (req, 
         console.error('Error sending CSP Report:', error);
         res.status(400).json({ error: 'Error sending CSP Report' });
     });
-});
-
-// Endpoint do przesyłania danych do Google Sheets
-app.post('/api/submit-data', async (req, res) => {
-    const dataToSend = req.body.values;
-    console.log("Received data to submit:", dataToSend); // Logowanie danych
-    console.log("CLIENT_ID:", process.env.CLIENT_ID); // Logowanie CLIENT_ID
-    console.log("SPREADSHEET_ID:", process.env.SPREADSHEET_ID); // Logowanie SPREADSHEET_ID
-    console.log("REFRESH_TOKEN:", process.env.REFRESH_TOKEN); // Dodajmy logowanie REFRESH_TOKEN (bezpieczne do celów diagnostycznych)
-
-    try {
-        const response = await axios.post(
-            `https://sheets.googleapis.com/v4/spreadsheets/${process.env.SPREADSHEET_ID}/values/scaned_products!A:F:append?valueInputOption=USER_ENTERED&insertDataOption=INSERT_ROWS`,
-            { values: dataToSend },
-            {
-                headers: {
-                    'Authorization': `Bearer ${process.env.ACCESS_TOKEN}`, // Użycie ACCESS_TOKEN jako nagłówka autoryzacji
-                    'Content-Type': 'application/json'
-                }
-            }
-        );
-        console.log("Data submitted successfully:", response.data); // Logowanie odpowiedzi
-        res.status(200).json({ message: 'Data submitted successfully!', response: response.data });
-    } catch (error) {
-        console.error('Error submitting data:', error.response ? error.response.data : error.message); // Logowanie błędu
-        res.status(500).json({ error: 'Error submitting data.' });
-    }
 });
 
 // Endpoint do sprawdzania zdrowia serwera
